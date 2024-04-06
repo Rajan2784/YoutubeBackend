11414bbafb414ef9a396f68c1697b85a4f67d12b
Rajan2784 <rajansingh6616@gmail.com>
2024-04-06 18:25:02 +0530

    models created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server is running at port ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed :", err);
+  });
